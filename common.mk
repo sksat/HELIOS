e83771fab95fd8948007b2b582b3c7b1440bd9f5
sksat <sksat.tec@gmail.com>
2018-09-02 21:29:06 +0900

    print commit id & date

--- common.mk
+++ common.mk
@@ -1,11 +1,16 @@
 ARCH		:= x86_64
 BOOT_TYPE	:= UEFI
 
+GIT_COMMIT_ID  := $(shell git log -1 --format='%H')
+GIT_COMMIT_DATE:= $(shell git log -1 --format='%ad')
+
 CC			:= clang
 CXX			:= clang++
 LD			:= lld-link
 
-CFLAGS		= -Wall -nostdlibinc
+CFLAGS		= -Wall -nostdlibinc \
+			  -DGIT_COMMIT_ID="\"$(GIT_COMMIT_ID)\"" \
+			  -DGIT_COMMIT_DATE="\"$(GIT_COMMIT_DATE)\""
 LDFLAGS		= -nodefaultlib
 
 ifeq ($(BOOT_TYPE),UEFI)
@@ -17,4 +22,4 @@ endif
 	$(CC) $(CFLAGS) -c -o $@ $<
 
 %.o: %.cc
-	$(CXX) $(CFLAGS) -c -o $@ $<
+	$(CXX) -std=c++1z $(CFLAGS) -c -o $@ $<
--- src/efimain.cc
+++ src/efimain.cc
@@ -3,6 +3,7 @@
 void efi_main(void *ImageHandle, EFI::SYSTEM_TABLE *system_table){
 	auto con_out = system_table->con_out;
 	con_out->clear_screen(con_out);
-	con_out->output_string(con_out, (wchar_t*)L"Hello, UEFI!");
+	con_out->output_string(con_out, (wchar_t*)L"Hello, UEFI!\n\rcommit: ");
+	con_out->output_string(con_out, (wchar_t*)L"" GIT_COMMIT_ID "(" GIT_COMMIT_DATE ")");
 	while(true);
 }
